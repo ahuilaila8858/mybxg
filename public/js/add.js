264bae3c3bfd0f743bd1d2cc57b2a588b6bde0e3
六六六 <304465638@qq.com>
2018-01-04 07:57:19 +0800

    完成讲师添加和编辑功能

--- public/js/add.js
+++ public/js/add.js
@@ -12,6 +12,7 @@ define(["jquery", "template", "until"], function($, template, until) {
                     data.result.operate = "编辑讲师"
                     var html = template("teacherTpl", data.result);
                     $("#teacherInfo").html(html);
+                    submitForm("/api/teacher/update")
                 }
             }
         })
@@ -19,5 +20,22 @@ define(["jquery", "template", "until"], function($, template, until) {
         // 添加页面
         var html = template("teacherTpl", { operate: "添加讲师" });
         $("#teacherInfo").html(html);
+        submitForm("/api/teacher/add")
+    }
+
+    function submitForm(url) {
+        $("#teacherBtn").click(function() {
+            $.ajax({
+                type: "post",
+                url: url,
+                data: $("#teacherForm").serialize(),
+                dataType: 'json',
+                success: function(data) {
+                    if (data.code == 200) {
+                        location.href = "/teacher/list";
+                    }
+                }
+            })
+        })
     }
 })
