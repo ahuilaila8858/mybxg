3cc71b5c3d76c2799586e9a505194ce4c9881e05
六六六 <304465638@qq.com>
2018-01-07 16:00:47 +0800

    完成setting分支功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(["jquery", "template", "ckeditor", "uploadify", "region", "datepicker", "language"], function($, template, CKEDITOR) {
+define(["jquery", "template", "ckeditor", "uploadify", "region", "datepicker", "language", "validate", "form"], function($, template, CKEDITOR) {
     $.ajax({
         type: "post",
         url: "/api/teacher/profile",
@@ -7,6 +7,7 @@ define(["jquery", "template", "ckeditor", "uploadify", "region", "datepicker", "
             if (data.code == 200) {
                 var html = template("settingsTpl", data.result);
                 $("#settingsInfo").html(html);
+                // 上传头像
                 $("#upfile").uploadify({
                     fileObjName: "tc_avatar",
                     swf: "/public/assets/uploadify/uploadify.swf",
@@ -16,14 +17,39 @@ define(["jquery", "template", "ckeditor", "uploadify", "region", "datepicker", "
                         console.log(filea)
                     }
                 });
+                // 省市县三级联动
                 $("#pcd").region({
                     url: "/public/assets/jquery-region/region.json"
                 });
+                // 富文本
                 CKEDITOR.replace("ckeditor", {
                     toolbarGroups: [
                         { name: 'clipboard', groups: ['clipboard', 'undo'] },
                         { name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
                     ]
+                });
+                $("#settingsForm").validate({
+                    sendForm: false,
+                    valid: function() {
+                        var p = $("#p").find("option:selected").text()
+                        var c = $("#c").find("option:selected").text()
+                        var d = $("#d").find("option:selected").text()
+                        var homeTown = p + "|" + c + "|" + d;
+                        for (var instance in CKEDITOR.instances) {
+                            CKEDITOR.instances[instance].updateElement();
+                        }
+                        $(this).ajaxSubmit({
+                            type: "post",
+                            url: "/api/teacher/modify",
+                            data: { tc_hometown: homeTown },
+                            dataType: "json",
+                            success: function(data) {
+                                if (data.code == 200) {
+                                    location.reload()
+                                }
+                            }
+                        })
+                    }
                 })
             }
         }
